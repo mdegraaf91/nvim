076f7b05c6405a8002cbff5f96c5a1983dca3b95
Michael <mdegraaf91@gmail.com>
2024-02-26 20:08:07 +0100

    add lazygit keymap: <leader>gg

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,6 +11,9 @@ return {
         "tpope/vim-fugitive",
     },
     {
-        "kdheepak/lazygit.nvim"
+        "kdheepak/lazygit.nvim",
+        config = function()
+            vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", {})
+        end
     }
 }
