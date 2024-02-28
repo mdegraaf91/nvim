533f48e38f821c05226f9c6725b23146da846d5f
Michael <mdegraaf91@gmail.com>
2024-02-28 18:29:30 +0100

    gruvbox-baby lualine theme

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         require("lualine").setup({
             options = {
-                theme = 'codedark'
+                theme = 'gruvbox-baby'
             }
         })
     end
