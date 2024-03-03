4c956749582ec96f543ba1b05bbeba424f30ffc2
Michael <mdegraaf91@gmail.com>
2024-03-03 07:35:19 +0100

    harpoon

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,11 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" }
+                ensure_installed = {
+                    "lua_ls",
+                    "tsserver",
+                    "intelephense",
+                }
             })
         end
     },
@@ -19,6 +23,7 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.tsserver.setup({})
+            lspconfig.intelephense.setup({})
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
