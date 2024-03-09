b55372e82cedd0fa25e247efc3600f48062f53c5
Michael <mdegraaf91@gmail.com>
2024-03-09 16:47:44 +0100

    none-ls (formatter + linter)

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,26 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting
+
+		null_ls.setup({
+			sources = {
+				formatting.stylua,
+				formatting.prettierd.with({
+					filetypes = {
+						"css",
+						"scss",
+						"html",
+						"json",
+						"yaml",
+						"markdown",
+                        "php",
+					},
+				}),
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
