b6fec330308e416c173fe1c75fac78d7794ceb87
Michael <mdegraaf91@gmail.com>
2024-04-02 21:24:17 +0200

    obsidian

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,24 @@
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    config = function()
+        require("obsidian").setup({
+            dir = "~/Vault/Vault",
+            notes_subdir = "0 Inbox",
+            new_notes_location = "notes_subdir",
+            picker = { name = "telescope.nvim" },
+            note_id_func = function(title)
+                return title
+            end,
+            note_path_fun = function(spec)
+                local path = spec.dir / tostring(spec.title)
+
+                return path:with_suffix(".md")
+            end
+        })
+
+        vim.keymap.set('n', "<leader>n", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
+    end
+}
