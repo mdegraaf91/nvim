513607a4886fc0ee8f3662cb413cefe7bb9dfb7c
Michael <contact@michaeldegraaf.dev>
2024-06-23 06:50:18 +0200

    better comments lines

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,22 +1,45 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup()
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
 
-            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-        end
-    },
-    {
-        "tpope/vim-fugitive",
-    },
-    {
-        "tpope/vim-commentary",
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        config = function()
-            vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", {})
-        end
-    }
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"tpope/vim-commentary",
+		config = function()
+			local ts_utils = require("nvim-treesitter.ts_utils")
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "vue",
+				callback = function()
+					local function set_commentstring()
+						local node = ts_utils.get_node_at_cursor()
+						if not node then return end
+
+						local node_type = node:type()
+						if node_type == "template_element" then
+							vim.opt_local.commentstring = "<!-- %s -->"
+						end
+					end
+
+					vim.api.nvim_create_autocmd("CursorMoved", {
+						buffer = 0,
+						callback = set_commentstring,
+					})
+				end
+			})
+		end
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", {})
+		end,
+	},
 }
