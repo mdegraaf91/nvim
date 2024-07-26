2831b1a6fef45a67d83c950f5bd64a0ba1a84e8d
Michael <contact@michaeldegraaf.dev>
2024-07-26 09:50:11 +0200

    debugging: php

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,36 +1,55 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "nvim-neotest/nvim-nio",
-        "rcarriga/nvim-dap-ui",
-        "leoluz/nvim-dap-go",
-    },
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-        require("dap-go").setup()
-        require("dapui").setup()
+		require("dapui").setup()
+		require("dap-go").setup()
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
+		dap.adapters.php = {
+			type = "executable",
+			command = "node",
+			args = { "/Users/michael/.vscode/extensions/vscode-php-debug/out/phpDebug.js" },
+		}
+		dap.configurations.php = {
+			{
+				type = "php",
+				request = "launch",
+				name = "Listen for Xdebug",
+				port = 9003,
+			},
+		}
 
-        vim.keymap.set("n", "<Leader>dt", function()
-            dap.toggle_breakpoint()
-        end, {})
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 
-        vim.keymap.set("n", "<Leader>dc", function()
-            dap.continue()
-        end, {})
-    end,
+		vim.keymap.set("n", "<Leader>db", function()
+			dap.toggle_breakpoint()
+		end, {})
+
+		vim.keymap.set("n", "<Leader>dn", function()
+			dap.continue()
+		end, {})
+
+		vim.keymap.set("n", "<Leader>dt", function()
+			dapui.toggle()
+		end, {})
+
+	end,
 }
