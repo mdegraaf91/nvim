f9e515d613b25d173119ecfdc931c8c73f0ef436
Michael <contact@michaeldegraaf.dev>
2024-07-29 21:25:09 +0200

    telescope: find references <leader>fr

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
             vim.keymap.set("n", "<leader>fa", builtin.find_files, {})
             vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
         end
     },
     {
