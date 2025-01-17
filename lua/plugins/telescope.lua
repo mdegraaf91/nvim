4d79496c8f9e9083ebb7936eba2aa5fed813660a
Michael <mdegraaf91@gmail.com>
2025-01-17 12:28:38 +0100

    gd: go to definition

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
             vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
-            vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, {})
+            vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         end
     },
