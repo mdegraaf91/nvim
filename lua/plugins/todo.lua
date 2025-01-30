85e46640eebd60290d77ecc02b1279099adeb02c
Michael <michael@MacBook-Pro-van-Michael.local>
2025-01-30 16:45:53 +0100

    todo in telescope

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,8 +1,7 @@
 return {
-    "folke/todo-comments.nvim",
-    config = function()
-        require("todo-comments").setup()
-
-        vim.keymap.set('n', "<leader>to", '<cmd>TodoTrouble<CR>', {})
-    end
+  "folke/todo-comments.nvim",
+  config = function()
+    require("todo-comments").setup()
+    vim.keymap.set('n', "<leader>to", '<cmd>TodoTelescope<CR>', {})
+  end
 }
