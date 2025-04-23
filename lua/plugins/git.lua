b806323c59fa951bf5ff9198f91535dbbe27b186
Michael <michael@MacBook-Pro-van-Michael.local>
2025-04-23 17:19:38 +0200

    remove unused code

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,32 +17,7 @@ return {
     end
 	},
 	{
-		"tpope/vim-commentary",
-		config = function()
-
--- NOTE: this causes big lag
-
---			local ts_utils = require("nvim-treesitter.ts_utils")
--- 			vim.api.nvim_create_autocmd("FileType", {
--- 				pattern = "vue",
--- 				callback = function()
--- 					local function set_commentstring()
--- 						local node = ts_utils.get_node_at_cursor()
--- 						if not node then return end
-
--- 						local node_type = node:type()
--- 						if node_type == "template_element" then
--- 							vim.opt_local.commentstring = "<!-- %s -->"
--- 						end
--- 					end
-
--- 					vim.api.nvim_create_autocmd("CursorMoved", {
--- 						buffer = 0,
--- 						callback = set_commentstring,
--- 					})
--- 				end
--- 			})
-		end
+		"tpope/vim-commentary"
 	},
 	{
 		"kdheepak/lazygit.nvim",
