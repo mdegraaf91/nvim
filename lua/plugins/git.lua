f3df1a954416e17b3b6a22532b403e363284f722
Michael <michael@MacBook-Pro-van-Michael.local>
2025-04-23 17:19:57 +0200

    gitsigns: diffthis + blame_line

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,6 +7,8 @@ return {
 
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 			vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gl", ":Gitsigns blame_line<CR>", {})
+			vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis<CR>", {})
       vim.opt.signcolumn="yes"
 		end,
 	},
