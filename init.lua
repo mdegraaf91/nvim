58b12c6f8bb742a65b7b2075cb60a37dbe5cc64e
Michael <mdegraaf91@gmail.com>
2025-05-23 20:19:00 +0200

    neotree toggle off

--- init.lua
+++ init.lua
@@ -29,9 +29,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.highlight.on_yank { higroup = 'Visual', timeout = 200 }
     end,
 })
-
-vim.api.nvim_create_autocmd('VimEnter', {
-    callback = function()
-      vim.cmd ":Neotree toggle"
-    end
-})
