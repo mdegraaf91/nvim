a59e668d02d78c180710cc32905d6b81e781d770
Michael <mdegraaf91@gmail.com>
2025-05-23 20:18:06 +0200

    lualine update

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -7,6 +7,14 @@ return {
         require("lualine").setup({
             options = {
                 theme = 'gruvbox-baby'
+            },
+            sections = {
+              lualine_a = {'mode'},
+              lualine_b = {'branch', 'diff', 'diagnostics'},
+              lualine_c = {'filename'},
+              lualine_x = {'filetype'},
+              lualine_y = {'progress'},
+              lualine_z = {'location'}
             }
         })
     end
